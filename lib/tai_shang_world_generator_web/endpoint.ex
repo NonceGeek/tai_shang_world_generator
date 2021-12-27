00bf9c43dfbc33d5252b4428b0daacefec2e2258
leeduckgo <albertschr@163.com>
2021-12-27 13:18:03 +0800

    feat/add cros

--- lib/tai_shang_world_generator_web/endpoint.ex
+++ lib/tai_shang_world_generator_web/endpoint.ex
@@ -12,6 +12,9 @@ defmodule TaiShangWorldGeneratorWeb.Endpoint do
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
+  # IMPORTANT!
+  plug CORSPlug
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest

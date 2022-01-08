dceebf664b63f7a0775c9e2d5a5e768a36f00955
leeduckgo <albertschr@163.com>
2022-01-08 19:43:09 +0800

    feat/update generator api

--- lib/tai_shang_world_generator_web/controllers/generator_controller.ex
+++ lib/tai_shang_world_generator_web/controllers/generator_controller.ex
@@ -1,9 +1,13 @@
 defmodule TaiShangWorldGeneratorWeb.GeneratorController do
   alias TaiShangWorldGenerator.{BlockchainFetcher, MapTranslator}
+  alias TaiShangWorldGenerator.NftInteractor
   alias TaiShangWorldGeneratorWeb.ResponseMod
   alias Utils.TypeTranslator
 
   use TaiShangWorldGeneratorWeb, :controller
+
+  @default_contract_addr "0x545EDf91e91b96cFA314485F5d2A1757Be11d384"
+  @default_rule "RuleA"
 
   def gen(conn, params) do
     params_atom =  ExStructTranslator.to_atom_struct(params)
@@ -17,5 +21,20 @@ defmodule TaiShangWorldGeneratorWeb.GeneratorController do
   }) do
     payload = MapTranslator.get_map_by_block_num_and_rule_name(block_num, rule_name)
     json(conn, ResponseMod.get_res(payload, :ok))
+  end
+
+  def do_gen(conn, %{
+    contract_id: contract_id,
+    token_id: token_id
+  }) do
+    abstract_map =
+      @default_contract_addr
+      |> NftInteractor.get_block_height_for_token(
+        String.to_integer(token_id)
+      )
+      |> MapTranslator.get_map_by_block_num_and_rule_name(
+        @default_rule
+        )
+    json(conn, ResponseMod.get_res(abstract_map, :ok))
   end
 end

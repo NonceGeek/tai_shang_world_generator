1c30c541b16cd68898263472cc9e9b6fe8c6bbe6
Ken Chen <chengusky@gmail.com>
2022-01-22 23:48:07 +0800

    #40 Add game_event api

---
+++ lib/tai_shang_world_generator_web/controllers/admin_controller.ex
@@ -0,0 +1,17 @@
+defmodule TaiShangWorldGeneratorWeb.AdminController do
+  alias TaiShangWorldGeneratorWeb.ResponseMod
+  alias TaiShangWorldGenerator.GameEvent
+  use TaiShangWorldGeneratorWeb, :controller
+
+  def game_event(conn, %{ "admin_key" => client_admin_key, "payload" => payload } = _params) do
+    sys_admin_key = System.fetch_env!("ADMIN_KEY")
+
+    if client_admin_key == sys_admin_key do
+      {:ok, event} = GameEvent.create(ExStructTranslator.to_atom_struct(payload))
+      json(conn, ResponseMod.get_res(%{ msg: "success" }, :ok))
+    else
+      json(conn, ResponseMod.get_res(%{ error_msg: "Unauthenticated" }, :error))
+    end
+  end
+
+end

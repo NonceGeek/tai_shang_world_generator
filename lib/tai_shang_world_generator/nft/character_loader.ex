0fda392528f30968936d1c0ff37c5641cce29e4f
admin <admin@admindeMacBook-Pro.local>
2022-02-05 00:07:30 +0800

    impl load_character

--- lib/tai_shang_world_generator/nft/character_loader.ex
+++ lib/tai_shang_world_generator/nft/character_loader.ex
@@ -1,23 +1,59 @@
 defmodule TaiShangWorldGenerator.Nft.CharacterLoader do
+  alias TaiShangWorldGenerator.NftInteractor
+  alias TaiShangWorldGenerator.Chain
+  @keys [
+    :first,
+    :second,
+    :third,
+    :fourth,
+    :fifth,
+    :sixth
+  ]
 
-  def load_character(chain, contract_addr, token_id) do
+  def load_character(%Chain{endpoint: endpoint}, contract_addr, token_id) do
     # TODO, just Mock it
     # See Parser in:
     # https://github.com/WeLightProject/tai_shang_nft_gallery/blob/main/lib/tai_shang_nft_gallery/nft/parser.ex
-    badges_list = ["noncegeeker", "artist"]
-    elements = %{
-      first: 1,
-      second: 1,
-      third: 1,
-      fourth: 1,
-      fifth: 1,
-      sixth: 1
-    }
+    token_uri(contract_addr, token_id, [url: endpoint])
+  end
 
-    %{
-      badges: badges_list,
-      elements: elements
+  def token_uri(contract_addr, token_id, endpoint \\ []) do
+    %{img_parsed: img_parsed} = NftInteractor.token_uri(contract_addr, token_id, endpoint)
+    IO.inspect(img_parsed)
+    %{ elements: get_nums_in_svg(img_parsed),
+       badges: get_badges_list(img_parsed)
     }
+  end
 
+  def get_badges_list(img_parsed) do
+    value =
+      img_parsed
+      |> String.split("Badges:")
+      |> Enum.drop(1)
+      |> Enum.map(&(String.split(&1, "class=\"base\">") |> Enum.at(1)))
+      |> Enum.map(&(String.split(&1, "</text") |> Enum.at(0)))
+      |> Enum.at(0)
+      |> String.trim
+      cond do
+        value == "" ->
+           []
+        true ->
+         Jason.decode!(value)
+    end
+  end
+
+  def get_nums_in_svg(img_parsed) do
+    value =
+      img_parsed
+      |> String.split("class=\"base\">")
+      |> Enum.drop(1)
+      |> Enum.map(&String.at(&1, 0))
+      |> Enum.reject(&(Integer.parse(&1) == :error))
+      |> Enum.map(&String.to_integer(&1))
+
+    # value
+    @keys
+    |> Enum.zip(value)
+    |> Enum.into(%{})
   end
 end
